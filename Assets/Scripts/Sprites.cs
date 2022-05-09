45bc1a1861aadb5e77914637a19e17b15fcb01a7
Brian Aston <astonb.dev@gmail.com>
2022-05-09 16:59:50 -0700

    Working on programmatically getting my sprites

--- Assets/Scripts/Sprites.cs
+++ Assets/Scripts/Sprites.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Sprites : MonoBehaviour
 {
-    public Sprites[] sprites = new Sprite[6];
+    public Sprite[] sprites = new Sprite[6];
+
+    private void Start()
+    {
+        for(int i = 2; i<=7; i++){
+            sprites[i-2] = Resources.Load(
+                "images/free-fruit-vector_0{i}") as Sprite;
+        }
+
+
+        foreach(Sprite sprite in sprites){
+            Debug.Log(sprite);
+        }
+    }
 }
