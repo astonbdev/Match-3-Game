418ef92fa347e760d0ac5b117f32b95da92d1013
Brian Aston <astonb.dev@gmail.com>
2022-05-09 16:21:38 -0700

    Initial

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite sprite;
+    public int value;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
