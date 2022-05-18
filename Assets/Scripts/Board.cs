fcd52eb5d49b262cd2f9f93ff0c15ecd5d12f20c
Brian Aston <astonb.dev@gmail.com>
2022-05-17 20:02:29 -0700

    Fixing some syntax errors

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -14,7 +14,7 @@ public class Board : MonoBehaviour
 
         for(int i=0; i < 6; i++){
             for(int j=0; j < 6; j++){
-                board[i,j] = Instantiate(tile, new Vector3(0+i,0+j,0), Quaternion.identity);
+                board[i,j] = Instantiate(tile, new Vector3(0,0,0), Quaternion.identity);
             }
         }
     }
