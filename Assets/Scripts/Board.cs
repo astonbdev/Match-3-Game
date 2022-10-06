e35ff6bcea02b8e2d14859543b724baf1f47b25b
Brian Aston <astonb.dev@gmail.com>
2022-10-05 22:57:18 -0700

    Reworking the checkNeighbors() functionality. Could be running into an infinite loop

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -10,7 +10,7 @@ public class Board : MonoBehaviour
 
     void Start()
     {
-        board = new int[boardSize, boardSize];
+        board = new GameObject[boardSize, boardSize];
 
         GameObject tile = (GameObject)Resources.Load("prefabs/Tile", typeof(GameObject));
 
@@ -33,8 +33,15 @@ public class Board : MonoBehaviour
                 Debug.Log("board state" + board[i, j]);
             }
         }
+
+        StartCoroutine(DebugBoardState(2f));
     }
 
+    IEnumerator DebugBoardState(float waitTime){
+        yield return new WaitForSeconds(waitTime);
+
+        this.checkForMatches();
+    }
     //Swaps two passed tiles positions, reallocates neighbors
     public void swapTiles(GameObject tileOne, GameObject tileTwo)
     {
@@ -51,60 +58,76 @@ public class Board : MonoBehaviour
         //checkForMatches();
     }
 
-    private checkForMatches()
+    private void checkForMatches()
     {
-        HashSet<GameObject> matchedTiles;
-        //check each array of board
-        (int, int)[] scoredTileIndices;
+        HashSet<GameObject> matchedTiles = new HashSet<GameObject>();
 
         for (int i = 0; i < boardSize; i++)
         {
-            for (int j = 1; j < boardSize; j++)
+            for (int j = 0; j < boardSize; j++)
             {
                 List<GameObject> matchingTiles = this.checkNeighbors(i, j);
                 matchedTiles.UnionWith(matchingTiles);
             }
         }
-    }
 
-    private checkNeighbors(int row, int col)
-    {
-        GameObject currentTile = board[row][col];
-        int tileValue;
-        bool sameValue;
-        List<GameObject> matchingTiles;
-        //check southern neighbors
-        if (!(row + 1 >= boardSize))
-        {
-            southTile = board[row + 1][col];
-
-            currentTileValue = currentTile.GetComponent<Tile>().value();
-            southTileValue = southTile.GetComponent<Tile>().value();
-
-            if (currentTileValue == southTileValue)
-            {
-                matchingTiles.Add(currentTile);
-                matchingTiles.Add(southTile);
-            }
+        foreach(GameObject tile in matchedTiles){
+            Debug.Log("Tile Value: " + tile.GetComponent<Tile>().value);
+            tile.GetComponent<SpriteRenderer>().color = Color.blue;
         }
 
-        //check eastern neighbors
-        if (!(col + 1 >= boardSize))
-        {
-            eastTile = board[row + 1][col];
+        Debug.Log("MatchedTiles Length: " + matchedTiles.Count);
+    }
 
-            currentTileValue = currentTile.GetComponent<Tile>().value();
-            eastTileValue = eastTile.GetComponent<Tile>().value();
+    private List<GameObject> checkNeighbors(int row, int col)
+    {
+        GameObject currentTile = board[row, col];
+        int currTileValue = board[row, col].GetComponent<Tile>().value;
+        List<GameObject> matchingTiles = new List<GameObject>();
 
-            if (currentTileValue == eastTileValue)
-            {
-                matchingTiles.Add(currentTile);
-                matchingTiles.Add(eastTile);
+        List<List<GameObject>> neighbors = getTileNeighbors(row, col);
+
+        if(neighbors[0].Count != 0){
+            foreach(List<GameObject> neighborSet in neighbors){
+                int value1 = neighborSet[0].GetComponent<Tile>().value;
+                int value2 = neighborSet[1].GetComponent<Tile>().value;
+
+                if(value1 == currTileValue && value2 == currTileValue){
+                    foreach(GameObject neighbor in neighborSet){
+                        matchingTiles.Add(neighbor);
+                    }
+                }
             }
-
         }
 
         return matchingTiles;
+    }
+
+    /**
+        takes int row and col,
+        returns List of neighbors in board if within bounds
+    */
+    private List<List<GameObject>> getTileNeighbors(int row, int col){
+        List<List<GameObject>> neighbors = new List<List<GameObject>>
+        {
+            new List<GameObject>{}, 
+            new List<GameObject>{}
+        };
+
+        bool neighbor1 = !((row+2 >= boardSize) || (col+2 >= boardSize));
+        bool neighbor2 = !((row+1 >= boardSize) || (col+1 >= boardSize));
+
+        if(neighbor1 && neighbor2){
+            //add southern neighbors
+            Debug.Log("row/col: " + row + " " + col);
+            neighbors[0].Add(board[row + 1, col]);
+            neighbors[1].Add(board[row + 2, col]);
+            //add eastern neighbors
+            neighbors[0].Add(board[row, col + 1]);
+            neighbors[1].Add(board[row, col + 2]);
+        }
+
+        return neighbors;
     }
 
 
